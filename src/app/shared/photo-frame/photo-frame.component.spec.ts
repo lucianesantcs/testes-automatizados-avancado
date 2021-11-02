c1dbdeca0abe33ce0905730b5dbb55072e410670
Luciane Santos <lucianesantcs@gmail.com>
2021-11-02 19:10:07 -0300

    fix(testes-automatizados-avancado): corrigido lint aula 1

--- src/app/shared/photo-frame/photo-frame.component.spec.ts
+++ src/app/shared/photo-frame/photo-frame.component.spec.ts
@@ -3,19 +3,19 @@ import { PhotoFrameComponent } from "./photo-frame.component";
 import { PhotoFrameModule } from "./photo-frame.module";
 
 describe(PhotoFrameComponent.name, () => {
-    let fixture: ComponentFixture<PhotoFrameComponent> = null;
-    let component: PhotoFrameComponent;
+  let fixture: ComponentFixture<PhotoFrameComponent> = null;
+  let component: PhotoFrameComponent;
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            imports: [PhotoFrameModule]
-        }).compileComponents();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PhotoFrameModule],
+    }).compileComponents();
 
-        fixture = TestBed.createComponent(PhotoFrameComponent);
-        component = fixture.componentInstance;
-    });
+    fixture = TestBed.createComponent(PhotoFrameComponent);
+    component = fixture.componentInstance;
+  });
 
-    it('Should create component', () => {
-        expect(component).toBeTruthy();
-    });
-})
+  it("Should create component", () => {
+    expect(component).toBeTruthy();
+  });
+});
